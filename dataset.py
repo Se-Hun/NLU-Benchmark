ae1f84b963207f59d34adb6b07fbd1fe43a2bd4d
Se-Hun <sehunhu5247@gmail.com>
2021-08-09 15:24:02 +0900

    Add entity recognition

--- dataset.py
+++ dataset.py
@@ -5,6 +5,7 @@ from tqdm.auto import tqdm
 from transformers import PreTrainedTokenizer
 
 import torch
+from torch.nn import CrossEntropyLoss
 from torch.utils.data import Dataset, TensorDataset, DataLoader
 from pytorch_lightning import LightningDataModule
 
@@ -22,7 +23,7 @@ class IntentClassificationDataset(Dataset):
         mode: str = "train"
     ):
         """
-        `IntentClassificationDataset` read data files and convert to pytorch tensor.
+        `IntentClassificationDataset` read data files and convert to tensor dataset.
 
         Args:
             data_dir (str): path of dataset directory.
@@ -95,7 +96,8 @@ class IntentClassificationDataModule(LightningDataModule):
             data_name (str): dataset name, e.g., "atis"
             model_type (str): model type, e.g., "bert"
             model_name (str): name of the specific model, e.g., "bert-base-cased"
-            max_seq_length (int, optional): the maximum length (in number of tokens) for the inputs to the transformer model. defaults to 128.
+            max_seq_length (int, optional): the maximum length (in number of tokens) for the inputs to the transformer model.
+                                            defaults to 128.
             batch_size (int, optional): batch size for single GPU. defaults to 8.
         """
 
@@ -143,7 +145,6 @@ class IntentClassificationDataModule(LightningDataModule):
                                                         self.max_seq_length, self.intent_vocab, "train")
             val_dataset = IntentClassificationDataset(self.data_dir, self.tokenizer,
                                                       self.max_seq_length, self.intent_vocab, "valid")
-
             self.train_dataset = train_dataset
             self.val_dataset = val_dataset
 
@@ -151,7 +152,202 @@ class IntentClassificationDataModule(LightningDataModule):
         if stage == 'test' or stage is None:
             test_dataset = IntentClassificationDataset(self.data_dir, self.tokenizer,
                                                        self.max_seq_length, self.intent_vocab, "test")
+            self.test_dataset = test_dataset
 
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True)
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    @staticmethod
+    def get_supported_dataset() -> List[str]:
+        """
+        This function returns list of supported dataset name by this data module.
+
+        Returns:
+            List[str]: list of supported dataset name.
+        """
+        return ["atis", "snips"]
+
+
+class EntityRecognitionDataset(Dataset):
+    def __init__(
+        self,
+        data_dir: str,
+        tokenizer: PreTrainedTokenizer,
+        max_seq_length: int,
+        entity_vocab: dict,
+        mode: str = "train"
+    ):
+        """
+        `EntityRecognitionDataset` read data files and convert to tensor dataset.
+
+        Args:
+            data_dir (str): path of dataset directory.
+            tokenizer (PreTrainedTokenizer): huggingface's PreTrainedTokenizer object.
+            max_seq_length (int): the maximum length (in number of tokens) for the inputs to the transformer model.
+            entity_vocab (dict): vocabulary of entity.
+            mode (str, optional): type of current dataset, e.g. "train", "valid", "test". defaults to "train".
+        """
+        # load text data and label
+        text_data = load_txt(os.path.join(data_dir, mode, "seq.in"))
+        entities_data = load_txt(os.path.join(data_dir, mode, "seq.out"))
+
+        # check number of data
+        assert len(text_data) == len(entities_data), "text and entity must have the same number of elements!"
+
+        # get ignore_index
+        pad_token_label_id = CrossEntropyLoss().ignore_index
+
+        # convert data to features
+        features = []
+        for idx, (text, entities_str) in tqdm(enumerate(zip(text_data, entities_data)), total=len(text_data)):
+            tokens = []
+            label_ids = []
+
+            words = text.split()
+            labels = entities_str.split()
+            for word, label in zip(words, labels):
+                word_tokens = tokenizer.tokenize(word)
+                if not word_tokens:
+                    word_tokens = [tokenizer.unk_token]  # For handling the bad-encoded word
+                tokens.extend(word_tokens)
+                # Use the real label id for the first token of the word, and padding ids for the remaining tokens
+                label_ids.extend([entity_vocab[label]] + [pad_token_label_id] * (len(word_tokens) - 1))
+
+            special_tokens_count = 2
+            if len(tokens) > max_seq_length - special_tokens_count:
+                tokens = tokens[:(max_seq_length - special_tokens_count)]
+                label_ids = label_ids[:(max_seq_length - special_tokens_count)]
+
+            # Add [SEP]
+            tokens += [tokenizer.sep_token]
+            label_ids += [pad_token_label_id]
+
+            # Add [CLS]
+            tokens = [tokenizer.cls_token] + tokens
+            label_ids = [pad_token_label_id] + label_ids
+
+            token_type_ids = [0] * len(tokens)
+
+            input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+            attention_mask = [1] * len(input_ids)
+
+            padding_length = max_seq_length - len(input_ids)
+            input_ids += [tokenizer.pad_token_id] * padding_length
+            attention_mask += [0] * padding_length
+            token_type_ids += [0] * padding_length
+            label_ids += [pad_token_label_id] * padding_length
+
+            assert len(input_ids) == max_seq_length
+            assert len(attention_mask) == max_seq_length
+            assert len(token_type_ids) == max_seq_length
+            assert len(label_ids) == max_seq_length
+
+            entities = label_ids
+
+            feature = {
+                "input_ids" : input_ids,
+                "token_type_ids" : token_type_ids,
+                "attention_mask" : attention_mask,
+                "entities" : entities
+            }
+            features.append(feature)
+
+        self.features = features
+
+        # convert features to TensorDataset
+        all_input_ids = torch.tensor([f["input_ids"] for f in features], dtype=torch.long)
+        all_attention_masks = torch.tensor([f["attention_mask"] for f in features], dtype=torch.long)
+        all_token_type_ids = torch.tensor([f["token_type_ids"] for f in features], dtype=torch.long)
+        all_entities = torch.tensor([f["entities"] for f in features], dtype=torch.long)
+
+        dataset = TensorDataset(all_input_ids, all_attention_masks, all_token_type_ids, all_entities)
+        self.dataset = dataset
+
+    def __getitem__(self, idx: int):
+        return self.dataset[idx]
+
+    def __len__(self):
+        return len(self.dataset)
+
+class EntityRecognitionDataModule(LightningDataModule):
+    def __init__(
+        self,
+        data_name: str,
+        model_type: str,
+        model_name: str,
+        max_seq_length: int = 128,
+        batch_size: int = 8
+    ):
+        """
+        `EntityRecognitionDataModule` prepare data and data loaders for entity recognition.
+
+        Args:
+            data_name (str): dataset name, e.g., "atis"
+            model_type (str): model type, e.g., "bert"
+            model_name (str): name of the specific model, e.g., "bert-base-cased"
+            max_seq_length (int, optional): the maximum length (in number of tokens) for the inputs to the transformer model.
+                                            defaults to 128.
+            batch_size (int, optional): batch size for single GPU. defaults to 8.
+        """
+
+        super().__init__()
+
+        # configures
+        self.data_name = data_name.lower()
+        if self.data_name not in self.get_supported_dataset():
+            raise NotImplementedError(self.data_name)
+
+        self.model_type = model_type
+        self.model_name = model_name
+        self.do_lower_case = True if "uncased" in model_name else False
+
+        self.max_seq_length = max_seq_length
+        self.batch_size = batch_size
+
+        # for balancing between CPU and GPU
+        self.num_workers = 4 * torch.cuda.device_count()
+
+    def prepare_data(self) -> None:
+        # prepare data directory
+        self.data_dir = os.path.join("./", "data", self.data_name)
+
+        # load tokenizer
+        self.tokenizer = get_tokenizer(self.model_type, self.model_name, self.do_lower_case)
+
+        # load vocab of entity
+        self.entity_vocab = self._load_vocab(os.path.join(self.data_dir, "entity.vocab"))
+        self.num_entities = len(self.entity_vocab)
+
+    def _load_vocab(self, file_path: str) -> dict:
+        vocab = {}
+        with open(file_path, "r", encoding="utf-8") as f:
+            for line in f:
+                line = line.rstrip()
+                label, id = line.split("\t")
+                vocab[label] = int(id)
+        return vocab
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        # load train/val dataset objects
+        if stage == "fit" or stage is None:
+            train_dataset = EntityRecognitionDataset(self.data_dir, self.tokenizer,
+                                                     self.max_seq_length, self.entity_vocab, "train")
+            val_dataset = EntityRecognitionDataset(self.data_dir, self.tokenizer,
+                                                   self.max_seq_length, self.entity_vocab, "valid")
+            self.train_dataset = train_dataset
+            self.val_dataset = val_dataset
+
+        # load test dataset objects
+        if stage == 'test' or stage is None:
+            test_dataset = EntityRecognitionDataset(self.data_dir, self.tokenizer,
+                                                    self.max_seq_length, self.entity_vocab, "test")
             self.test_dataset = test_dataset
 
     def train_dataloader(self) -> DataLoader:
---
+++ run_entity_recognition.py
@@ -0,0 +1,267 @@
+import os
+import json
+import platform
+import argparse
+from glob import glob
+
+from seqeval import metrics
+from transformers import AdamW, get_linear_schedule_with_warmup
+
+import torch
+from torch.nn import CrossEntropyLoss
+from pytorch_lightning import LightningModule, Trainer, seed_everything
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+from pytorch_lightning.loggers import TensorBoardLogger
+
+from dataset import EntityRecognitionDataModule
+from util import ENTITY_MODEL_CLASSES, get_entity_model
+
+
+class EntityRecognizer(LightningModule):
+    def __init__(
+        self,
+        data_name: str,
+        model_type: str,
+        model_name: str,
+        entity_map: dict,
+        learning_rate: float = 5e-5
+    ):
+        """
+            `EntityRecognizer` run entity recognition.
+
+            Args:
+                data_name (str):
+                model_type (str):
+                model_name (str):
+                num_entities (int):
+                learning_rate (float, optional):
+            """
+        super().__init__()
+        self.save_hyperparameters()
+
+        # save entity_map
+        self.entity_map = entity_map
+        num_entities = len(entity_map)
+
+        # load model
+        model = get_entity_model(model_type, model_name, num_entities)
+        self.model = model
+
+    def forward(self, x):
+        return self.model(**x)
+
+    def training_step(self, batch, batch_idx):
+        inputs = {
+            "input_ids": batch[0],
+            "attention_mask": batch[1],
+            "token_type_ids": batch[2],
+            "labels": batch[3]
+        }
+
+        if self.hparams.model_type in ["distilbert"]:
+            del inputs["token_type_ids"]  # Distilbert don't use segment_ids.
+
+        outputs = self(inputs)
+
+        loss = outputs[0]
+        result = {"loss": loss}
+        return result
+
+    def validation_step(self, batch, batch_idx):
+        inputs = {
+            "input_ids": batch[0],
+            "attention_mask": batch[1],
+            "token_type_ids": batch[2],
+            "labels": batch[3]
+        }
+
+        if self.hparams.model_type in ["distilbert"]:
+            del inputs["token_type_ids"]  # Distilbert don't use segment_ids.
+
+        outputs = self(inputs)
+
+        loss, logits = outputs[:2]
+        preds = torch.argmax(logits, dim=2)
+
+        result = {"val_loss": loss, "preds": preds, "labels": batch[3]}
+        return result
+
+    def validation_epoch_end(self, outputs):
+        val_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
+        preds = torch.cat([x["preds"] for x in outputs]).detach().cpu().numpy()
+        labels = torch.cat([x["labels"] for x in outputs]).detach().cpu().numpy()
+
+        out_label_list = [[] for _ in range(labels.shape[0])]
+        preds_list = [[] for _ in range(preds.shape[0])]
+
+        pad_token_label_id = CrossEntropyLoss().ignore_index
+
+        label_map = {i: label for label, i in self.entity_map.items()}
+
+        for i in range(labels.shape[0]):
+            for j in range(labels.shape[1]):
+                if labels[i, j] != pad_token_label_id:
+                    out_label_list[i].append(label_map[labels[i][j]])
+                    preds_list[i].append(label_map[preds[i][j]])
+
+        val_f1 = metrics.f1_score(out_label_list, preds_list)
+
+        self.log("val_loss", val_loss, prog_bar=True)
+        self.log("val_f1", val_f1, prog_bar=True)
+        return {"val_loss": val_loss, "val_f1": val_f1}
+
+    def test_step(self, batch, batch_idx):
+        inputs = {
+            "input_ids": batch[0],
+            "attention_mask": batch[1],
+            "token_type_ids": batch[2],
+            "labels": batch[3]
+        }
+
+        if self.hparams.model_type in ["distilbert"]:
+            del inputs["token_type_ids"]  # Distilbert don't use segment_ids.
+
+        outputs = self(inputs)
+
+        _, logits = outputs[:2]
+        preds = torch.argmax(logits, dim=2)
+
+        result = {"preds": preds, "labels": batch[3]}
+        return result
+
+    def test_epoch_end(self, outputs):
+        preds = torch.cat([x["preds"] for x in outputs]).detach().cpu().numpy()
+        labels = torch.cat([x["labels"] for x in outputs]).detach().cpu().numpy()
+
+        out_label_list = [[] for _ in range(labels.shape[0])]
+        preds_list = [[] for _ in range(preds.shape[0])]
+
+        pad_token_label_id = CrossEntropyLoss().ignore_index
+
+        label_map = {i: label for label, i in self.entity_map.items()}
+
+        for i in range(labels.shape[0]):
+            for j in range(labels.shape[1]):
+                if labels[i, j] != pad_token_label_id:
+                    out_label_list[i].append(label_map[labels[i][j]])
+                    preds_list[i].append(label_map[preds[i][j]])
+
+        test_precision = metrics.precision_score(out_label_list, preds_list)
+        test_recall = metrics.recall_score(out_label_list, preds_list)
+        test_f1 = metrics.f1_score(out_label_list, preds_list)
+
+        results = {"precision": test_precision, "recall": test_recall, "f1": test_f1}
+
+        result_file = os.path.join(self.trainer.checkpoint_callback.dirpath, "result.json")
+        with open(result_file, "w", encoding="utf-8") as f:
+            json.dump(results, f, ensure_ascii=False, indent=4)
+            print("Result file is dumped at ", result_file)
+
+        print(json.dumps(results, indent=4))
+        return results
+
+    def configure_optimizers(self):
+        no_decay = ['bias', 'LayerNorm.weight']
+        optimizer_grouped_parameters = [
+            {'params': [p for n, p in self.model.named_parameters() if not any(nd in n for nd in no_decay)],
+             'weight_decay': 0.0},
+            {'params': [p for n, p in self.model.named_parameters() if any(nd in n for nd in no_decay)],
+             'weight_decay': 0.0}
+        ]
+        optimizer = AdamW(optimizer_grouped_parameters, lr=self.hparams.learning_rate, eps=1e-8)
+
+        t_total = len(self.train_dataloader()) // self.trainer.max_epochs
+        scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=t_total)
+
+        return [optimizer], [scheduler]
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = argparse.ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument('--learning_rate', type=float, default=5e-5)
+        return parser
+
+def main():
+    # Argument Setting -------------------------------------------------------------------------------------------------
+    parser = argparse.ArgumentParser()
+
+    # Required parameters
+    parser.add_argument("--model_type", type=str, required=True,
+                        help="Model type selected in the list: " + ", ".join(ENTITY_MODEL_CLASSES.keys()))
+    parser.add_argument("--model_name", type=str, required=True,
+                        help="Model name of pre-trained model. you can search at huggingface models.")
+    parser.add_argument("--data_name", type=str, required=True,
+                        help="Data name selected in the list: " + ", ".join(
+                            EntityRecognitionDataModule.get_supported_dataset()))
+
+    # Other parameters
+    parser.add_argument("--do_train", action="store_true", help="Whether to run training.")
+    parser.add_argument("--do_eval", action="store_true", help="Whether to run eval on the dev set.")
+
+    parser.add_argument("--max_seq_length", default=128, type=int,
+                        help="The maximum total input sequence length after WordPiece tokenization. Sequences "
+                             "longer than this will be truncated, and sequences shorter than this will be padded.")
+
+    parser.add_argument("--num_train_epochs", default=10, type=int, help="Epochs at train time.")
+    parser.add_argument("--batch_size", default=32, type=int, help="Batch size")
+    parser.add_argument("--gpu_id", type=str, default="0",
+                        help="Gpu device id.")
+
+    parser.add_argument("--seed", default=42, type=int, help="Seed number")
+
+    parser = Trainer.add_argparse_args(parser)
+    parser = EntityRecognizer.add_model_specific_args(parser)
+    args = parser.parse_args()
+    # ------------------------------------------------------------------------------------------------------------------
+
+    # set seed
+    seed_everything(args.seed)
+
+    # load DataModule
+    args.model_type = args.model_type.lower()
+    args.model_name = args.model_name.lower()
+    args.data_name = args.data_name.lower()
+    dm = EntityRecognitionDataModule(args.data_name, args.model_type, args.model_name,
+                                     args.max_seq_length, args.batch_size)
+    dm.prepare_data()
+    entity_map = dm.entity_vocab
+
+    # load Callbacks and Loggers
+    model_dir = './model/{}/{}/{}'.format(args.data_name, "entity", args.model_name.replace("/", "-"))
+    model_checkpoint_callback = ModelCheckpoint(
+        monitor='val_f1',  # or 'val_loss'
+        mode='max',  # or 'min'
+        dirpath=model_dir,
+        filename='{epoch:02d}-{val_loss:.2f}'
+    )
+
+    tensorboard_logger = TensorBoardLogger(
+        save_dir=model_dir, name=''  # <-- if experiment name(=name) is empty, sub directory is not made.
+    )
+
+    # load Trainer
+    trainer = Trainer(
+        gpus=args.gpu_id if platform.system() != 'Windows' else 1,  # <-- for dev. pc
+        logger=tensorboard_logger,
+        callbacks=[model_checkpoint_callback],
+        max_epochs=args.num_train_epochs
+    )
+
+    # Do train !
+    if args.do_train:
+        model = EntityRecognizer(args.data_name, args.model_type, args.model_name, entity_map)
+        dm.setup('fit')
+        trainer.fit(model, dm)
+
+    # Do eval !
+    if args.do_eval:
+        model_files = glob(os.path.join(trainer.checkpoint_callback.dirpath, "*.ckpt"))
+        best_fn = sorted(model_files, key=lambda fn: fn.split("=")[-1])[0]
+        print("[Evaluation] Best Model File name is {}".format(best_fn))
+
+        model = EntityRecognizer.load_from_checkpoint(best_fn)
+        dm.setup('test')
+        trainer.test(model, datamodule=dm)
+
+if __name__ == '__main__':
+    main()
